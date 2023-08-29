321dc123be8156ba10faf9ff908835bee605365b
luis <fernando2001th@gmail.com>
2023-08-28 21:50:15 -0400

    Subida de archivos de inicializacion

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+<head>
+    <h1 style="text-align: center">Bienvenido al inicio</h1>
+    <button type="button" class="btn btn-primary">Prueba boostrap</button>
+</head>
+@endsection
